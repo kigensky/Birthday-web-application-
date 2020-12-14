64c4bee2a3c5b9ca3c0f4f998c48daf33151c3d0
victor kigen <vickigen@gmail.com>
2020-12-14 15:09:32 +0300

    Add week days, female and male names in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,8 @@
 document.getElementById("alert1").style.display = "none";
 document.getElementById("alert2").style.display = "none";
+
+var daysWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
