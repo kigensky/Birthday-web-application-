428e805aaac31c71070a334ba78ab688e6aa056e
victor kigen <vickigen@gmail.com>
2020-12-14 14:36:43 +0300

    Add first commit

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-function getDay(y, m, d) {
-    var days = ['Sunday', 'Monday', 'Tuesday',
-        'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var d = new Date(y, --m, d);
-    return d && days[d.getDay()];
-}
