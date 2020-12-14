1a23355280e1777927c01c9ee4c22e922cb5c66f
victor kigen <vickigen@gmail.com>
2020-12-14 15:13:41 +0300

    Add functions to be called in js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ var daysWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sat
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
+//functions to be called
+function getAkanName() {
+	var year = parseInt(document.getElementById("year").value);
+	var month = parseInt(document.getElementById("month").value);
+	var day = parseInt(document.getElementById("day").value);
+	var male = document.getElementById("male");
+	var female = document.getElementById("female");
