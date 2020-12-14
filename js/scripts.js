1bd7e99b164b303c2c44fe56313d2bfefea2748d
victor kigen <vickigen@gmail.com>
2020-12-14 15:18:33 +0300

    Add date validity functions and refresh function commits

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,29 @@ function getAkanName() {
 	var month = parseInt(document.getElementById("month").value);
 	var day = parseInt(document.getElementById("day").value);
 	var male = document.getElementById("male");
-	var female = document.getElementById("female");
+    var female = document.getElementById("female");
+    
+     
+    //date validity
+	if (day <= 0 || day >= 31) {
+		alert(
+			"INVALID DATE!"
+		);
+	}
+	var day = new Date(year + "/" + month + "/" + day);
+	//storing date in variable date of birth
+	var dateOfBirth = day.getDay();
+	if (male.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+		document.getElementById("span2").innerHTML =
+			"Your Akan Name is " + maleAkanNames[dateOfBirth];
+	} else if (female.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+        document.getElementById("span2").innerHTML = 
+        "Your Akan Name is " + femaleAkanNames[dateOfBirth];
+    }
+}
+//refresh button the whole site
+function refreshPage() {
+	window.location.reload();
+};
