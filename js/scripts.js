d74104b7878e7c87e2d1ef05eb84d8cdc4ae6b1b
victor kigen <vickigen@gmail.com>
2020-12-14 15:50:15 +0300

    commit all changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 document.getElementById("alert1").style.display = "none";
 document.getElementById("alert2").style.display = "none";
 
-var daysWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
 var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-//functions to be called
+var daysWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//calling functions
 function getAkanName() {
 	var year = parseInt(document.getElementById("year").value);
 	var month = parseInt(document.getElementById("month").value);
 	var day = parseInt(document.getElementById("day").value);
 	var male = document.getElementById("male");
-    var female = document.getElementById("female");
-    
-     
-    //date validity
+	var female = document.getElementById("female");
+
+	//validate date
 	if (day <= 0 || day >= 31) {
 		alert(
 			"INVALID DATE!"
@@ -32,9 +30,11 @@ function getAkanName() {
 		document.getElementById("alert1").style.display = "block";
         document.getElementById("span2").innerHTML = 
         "Your Akan Name is " + femaleAkanNames[dateOfBirth];
-    }
+	}
 }
 //refresh button the whole site
 function refreshPage() {
 	window.location.reload();
 };
+       
+        
